05bc46387585002ad73c9b211e7f013a47c084fa
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2023-11-07 11:14:24 +0200

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+const _ = require('lodash');
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player')
+
+const player = new Player(iframe);
+
+
+player.on('timeupdate', _.throttle(Data => {
+    const currentTime = Data.seconds;
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+}, 1000));
+
+const parsedCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(parsedCurrentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});

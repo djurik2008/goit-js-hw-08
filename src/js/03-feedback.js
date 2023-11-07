bb50065b595e5a767efb438448ac8e6c65fd9d2c
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2023-11-07 18:32:27 +0200

    completed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+const _ = require('lodash');
+const formEl = document.querySelector(".feedback-form")
 
+formEl.addEventListener('input',_.throttle(onFormInput, 500))
+formEl.addEventListener('submit', onFormSubmit)
+
+const lokalStorageKey = "feedback-form-state"
+
+function onFormInput(e) {
+    e.preventDefault()
+    const inputEl = formEl.elements.email
+    const messageEl = formEl.elements.message
+
+    const formData = {
+        email: inputEl.value,
+        message: messageEl.value
+    }
+    localStorage.setItem(lokalStorageKey, JSON.stringify(formData))
+}
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget);
+
+    const formDataObj = {};
+
+    formData.forEach((value, name) => {
+      formDataObj[name] = value;
+    });
+    console.log(formDataObj);
+  
+  e.currentTarget.reset();
+}
+
+const loadSavedData = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key))
+  } catch (error) {
+    return localStorage.getItem(key)
+  }
+}
+
+if (loadSavedData(lokalStorageKey) !== null) {
+  formEl.elements.email.value = loadSavedData(lokalStorageKey).email
+  formEl.elements.message.value = loadSavedData(lokalStorageKey).message
+}
